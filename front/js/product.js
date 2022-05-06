45364d6df59d210589ee323a616017fcb304547b
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-05-06 16:29:38 +0200

    Ajout de l'image + nom du produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,26 @@
 let params = new URLSearchParams(window.location.search);
 let idProduct = params.get('id');
 console.log(idProduct);
+
+fetch("http://localhost:3000/api/products")
+      .then(function(res){
+          if(res.ok){
+              return res.json();
+          }
+      })
+      .then(function (products) {
+          console.log(products);
+          for(let product of products){
+              if(idProduct == product._id){
+
+                let productImage = document.createElement("img");
+                productImage.setAttribute("src", product.imageUrl);
+                productImage.setAttribute("alt", product.altTxt);
+                document.querySelector(".item__img")
+                .appendChild(productImage);
+
+                document.querySelector("#title")
+                .textContent = product.name;
+              }
+          }
+      })
