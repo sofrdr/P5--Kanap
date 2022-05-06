a20498c1db4a7bf68d8f24a0e9731ec2792a41d7
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-05-06 16:49:37 +0200

    Ajout du prix + description + création fonction createColor()

--- front/js/product.js
+++ front/js/product.js
@@ -12,15 +12,36 @@ fetch("http://localhost:3000/api/products")
           console.log(products);
           for(let product of products){
               if(idProduct == product._id){
-
+                //Ajout de l'image du produit
                 let productImage = document.createElement("img");
                 productImage.setAttribute("src", product.imageUrl);
                 productImage.setAttribute("alt", product.altTxt);
                 document.querySelector(".item__img")
                 .appendChild(productImage);
 
+                //Ajout du nom du produit
                 document.querySelector("#title")
                 .textContent = product.name;
+
+                //Ajout du prix du produit
+                document.querySelector("#price")
+                .textContent = product.price;
+
+                //Ajout de la description du produit
+                document.querySelector("#description")
+                .textContent = product.description;
+
+                //Ajout des options de couleur
+                function createColor(valueColor){
+                    let option = document.createElement("option");
+                    option.setAttribute("value", valueColor);
+                    document.querySelector("#colors")
+                    .appendChild(option);
+                    option.textContent = valueColor;
+                }
+                for(color of product.colors){
+                    createColor(color);
+                }
               }
           }
       })
