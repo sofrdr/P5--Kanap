bf7b95479b4d2ff86e004d75a9fdf85ed229a28f
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-05-17 12:39:43 +0200

    Ajout d'une fonction getTotalPrice() qui calcule le prix total du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,85 +1,86 @@
 
 
 
-function getBasket(basket){
+function getBasket(basket) {
     const newBasketJson = localStorage.getItem("basket", basket);
     const newBasket = JSON.parse(newBasketJson);
     return newBasket;
-    
+
 }
 
 const myBasket = getBasket();
 console.log(myBasket)
 
-if(!myBasket){
+if (!myBasket) {
     alert("Votre panier est vide");
-}else{
+} else {
 
-for(let item of myBasket){
-    //Ajout d'une balise article pour chaque élement du panier
-    let article = document.createElement("article");
-    article.setAttribute("data-id", item.id);
-    article.setAttribute("data-color", item.color)
-    document.querySelector("#cart__items").appendChild(article);
+    for (let item of myBasket) {
+        //Ajout d'une balise article pour chaque élement du panier
+        let article = document.createElement("article");
+        article.setAttribute("data-id", item.id);
+        article.setAttribute("data-color", item.color)
+        document.querySelector("#cart__items").appendChild(article);
 
-    //Création de la div contenant l'image du produit
-    let divImg = document.createElement("div");
-    divImg.classList.add("cart__item__img");
-    let img = document.createElement("img");
-    article.appendChild(divImg);
-    divImg.appendChild(img);
+        //Création de la div contenant l'image du produit
+        let divImg = document.createElement("div");
+        divImg.classList.add("cart__item__img");
+        let img = document.createElement("img");
+        article.appendChild(divImg);
+        divImg.appendChild(img);
 
-    //Création de la div contenant la description du produit
-    let divContent = document.createElement("div");
-    divContent.classList.add("cart__item__content");
-    article.appendChild(divContent);
-    let divDescription = document.createElement("div");
-    divDescription.classList.add("cart__item__content__description");
-    divContent.appendChild(divDescription);
+        //Création de la div contenant la description du produit
+        let divContent = document.createElement("div");
+        divContent.classList.add("cart__item__content");
+        article.appendChild(divContent);
+        let divDescription = document.createElement("div");
+        divDescription.classList.add("cart__item__content__description");
+        divContent.appendChild(divDescription);
 
-    //Ajout des éléments de description
-    let name = document.createElement("h2");
-    let colorParagraph = document.createElement("p");
-    let priceParagraph = document.createElement("p");
-    divDescription.appendChild(name);
-    divDescription.appendChild(colorParagraph).textContent = item.color;
-    divDescription.appendChild(priceParagraph);
+        //Ajout des éléments de description
+        let name = document.createElement("h2");
+        let colorParagraph = document.createElement("p");
+        let priceParagraph = document.createElement("p");
+        divDescription.appendChild(name);
+        divDescription.appendChild(colorParagraph).textContent = item.color;
+        divDescription.appendChild(priceParagraph);
 
-    //Création des div et de leurs élements pour gérer la quantité
-    let divSettings = document.createElement("div");
-    divSettings.classList.add("cart__item__content__settings");
-    divContent.appendChild(divSettings);
-    let divQuantity = document.createElement("div");
-    divQuantity.classList.add("cart__item__content__settings__quantity");
-    divSettings.appendChild(divQuantity);
+        //Création des div et de leurs élements pour gérer la quantité
+        let divSettings = document.createElement("div");
+        divSettings.classList.add("cart__item__content__settings");
+        divContent.appendChild(divSettings);
+        let divQuantity = document.createElement("div");
+        divQuantity.classList.add("cart__item__content__settings__quantity");
+        divSettings.appendChild(divQuantity);
 
-    let quantityParagraph = document.createElement("p");
-    divQuantity.appendChild(quantityParagraph).textContent = "Qté : ";
-    let quantityInput = document.createElement("input");
-    divQuantity.appendChild(quantityInput);
-    quantityInput.setAttribute("type", "number");
-    quantityInput.setAttribute("name", "itemQuantity");
-    quantityInput.setAttribute("min", "1");
-    quantityInput.setAttribute("max", "100");
-    quantityInput.setAttribute("value", item.quantity);
-    quantityInput.classList.add("itemQuantity");
+        let quantityParagraph = document.createElement("p");
+        divQuantity.appendChild(quantityParagraph).textContent = "Qté : ";
+        let quantityInput = document.createElement("input");
+        divQuantity.appendChild(quantityInput);
+        quantityInput.setAttribute("type", "number");
+        quantityInput.setAttribute("name", "itemQuantity");
+        quantityInput.setAttribute("min", "1");
+        quantityInput.setAttribute("max", "100");
+        quantityInput.setAttribute("value", item.quantity);
+        quantityInput.classList.add("itemQuantity");
 
-    
-}
+
+    }
 }
 
 
 const dataID = document.querySelector("article").getAttribute("data-id");
-console.log(dataID) 
+console.log(dataID)
 
-async function getProducts(){
+
+async function getProducts() {
     const response = await fetch("http://localhost:3000/api/products");
     const products = await response.json();
-    console.log(products)
+    console.log(products);
 
-    for(let product of products){
+    for (let product of products) {
         //Si l'id d'un produit de l'API correspond à l'id du produit du panier
-        if(product._id == dataID){
+        if (product._id == dataID) {
             //On affiche l'image du produit du panier
             let img = document.querySelector(".cart__item__img img");
             img.setAttribute("src", product.imageUrl);
@@ -91,26 +92,60 @@ async function getProducts(){
         }
     }
 }
- getProducts()
+getProducts()
 
 
- let allQuantityInputs = document.querySelectorAll(".itemQuantity");
- let numberOfInputs = allQuantityInputs.length;
 
- 
- let totalQuantity = [];
- let sum = 0;
+let totalQuantity = [];
+let sum = 0;
 
-function getTotalproducts(){
-    for(let item of myBasket){
-        totalQuantity.push(Number(item.quantity));       
+/* Création d'une fonction getTotalProducts() pour obtenir le nombre total de produits
+ajoutés au panier */
+
+function getTotalproducts() {
+    //Pour chaque produit du panier, on récupère sa quantité et on l'ajoute dans un array
+    for (let item of myBasket) {
+        totalQuantity.push(Number(item.quantity));
     }
-    for(let i=0; i < totalQuantity.length; i++){
+    //On additionne chaque élément de cet array et on récupère le total dans une variable sum
+    for (let i = 0; i < totalQuantity.length; i++) {
         sum += totalQuantity[i];
     }
     return sum;
-} 
+}
 
 const totalProducts = getTotalproducts();
 console.log(totalProducts);
-                
+
+document.querySelector("#totalQuantity").textContent = totalProducts;
+
+
+/* Création d'une fonction getTotalPrice() pour calculer le prix total du panier */
+
+let totalPrice = 0;
+let price = [];
+async function getTotalPrice() {
+    const response = await fetch("http://localhost:3000/api/products");
+    const products = await response.json();
+    for (let item of myBasket) {        
+        for (let product of products) {
+            if (product._id == dataID) {
+                price.push(product.price);
+            }
+        }
+    }
+    console.log(price)
+    let quantity = [];
+    for (let item of myBasket) {
+        quantity.push(Number(item.quantity))
+    }
+    console.log(quantity)
+
+    for(let i = 0; i < quantity.length; i++){
+        totalPrice += (price[i] * quantity[i]);
+    }
+    console.log(totalPrice);
+    document.querySelector("#totalPrice").textContent = totalPrice;
+}
+
+getTotalPrice();
